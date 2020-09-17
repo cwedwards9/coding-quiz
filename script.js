2707ca7723284da725758fe5294fa12476c36418
Chase Edwards <chase.w.edwards9@gmail.com>
2020-09-17 16:29:42 -0400

    Adjust questionIndex variable, change event listener for next button, add array of questions/answers, add finish game function, add logic to dynamically create button choices for answers to the corresponding question

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
     
-    // hide question display on page load
+    var questionIndex = 0;
+
+    // hide question display and done display on page load
     $("#questionDisplay").hide();
+    $("#doneDisplay").hide();
 
 
     // clicking the start quiz button
@@ -22,28 +25,50 @@ $(document).ready(function(){
 
 
     // clicking any button choice will give us right/wrong and will retrieve next question
-    $(".btnChoice").on("click", function(){
+    $("div#choices").on("click", "button", function(){
         // retrieve question
-        var questionIndex = 1;
-        getQuestion(questionIndex);
         questionIndex++;
+        getQuestion(questionIndex);
     });
 
 
     // question function
     function getQuestion(index){
-        // populate question in question tag
 
+        // remove answers from last question
+        $("#choices").empty();
+
+        // Finish game
+        if(index > 9){
+            finishGame();
+            // timerCountdown();
+            return;
+        }
+
+        // populate question in question tag
         var currentQuestion = questions[index].question;
         $("#question").text(currentQuestion);
 
-        $("")
-           
-        
-        
-        
+        var currentChoices = questions[index].choices;
+
+        // create choices
+        for(var i = 0; i < currentChoices.length; i++){
+            var newChoice = $("<button>");
+            newChoice.addClass("btnChoice");
+            newChoice.attr("value", currentChoices[i]);
+            newChoice.text(currentChoices[i]);
+            
+            $("#choices").append(newChoice);
+        }
     }
 
+    function finishGame(){
+        // hide the question display
+        $("#questionDisplay").hide();
+
+        // show the all done display
+        $("#doneDisplay").show();
+    }
 
     // timer function
     function timerCountdown(){
@@ -62,21 +87,57 @@ $(document).ready(function(){
 
 
 
-
-
-
     // questions array
     var questions = [
         {
-            question: "What is the best programming language?",
-            choices: ["Java", "Go", "Ruby", "JavaScript"],
-            answer: "JavaScript"
+            question: "How do you make a comment in HTML?",
+            choices: ["//", "<!-- -->", "/* */", "/- -/"],
+            answer: "<!-- -->"
         },
         {
-            question: "Is CSS fun",
-            choices: ["Yes", "No" ],
-            answer: "No"
+            question: "Which element provides meta info and contains the <title> tag?",
+            choices: ["<body>", "<meta>", "<head>", "<html>"],
+            answer: "<head>"
         },
-    
+        {
+            question: "What is the name for the space between the content and the border within an element?",
+            choices: ["Margin", "Space", "Border-space", "Padding"],
+            answer: "Padding"
+        },
+        {
+            question: "Which of the following is a way to center an element:",
+            choices: ["float: center", "margin: 0 auto", "margin: center", "align: center"],
+            answer: "margin: 0 auto"
+        },
+        {
+            question: "In the Bootstrap framework, how many units does each row contain?",
+            choices: ["12", "10", "20", "8"],
+            answer: "12"
+        },
+        {
+            question: "Using the spacing utility in Bootstrap, how can we apply a size 5 margin to the bottom of an element?",
+            choices: ["mx-5", "margin-b-5", "mb-5", "m-bottom-5"],
+            answer: "mb-5"
+        },
+        {
+            question: "Which of the following is not a primitive data type in JavaScript?",
+            choices: ["String", "Null", "Undefined", "Decimal"],
+            answer: "Decimal"
+        },
+        {
+            question: 'x = 99. True or false: x == "99"',
+            choices: ["True", "False"],
+            answer: "True"
+        },
+        {
+            question: "Using vanilla JavaScipt, what method could we use to set the text of an element?",
+            choices: [".text", ".innerText", ".html", ".val"],
+            answer: ".innerText"
+        },
+        {
+            question: "How could we select an img tag using jQuery?",
+            choices: ['$("img")', '.querySelector("img")', '$(#img)', '$img'],
+            answer: '$("img")'
+        }
     ]
 });
